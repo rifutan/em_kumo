db79b674e68628a1272847c10df13bf3f15f7360
rifutan <rifutan@rifutan.local>
2016-05-18 22:30:35 +0900

    2015_10

---
+++ chara.js
@@ -0,0 +1,59 @@
+function chara_1() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">芥川聡介　<span style=\"font-size:20px\">キャスト：山田青空</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_1.png\" width=\"300px\"></div><div class=\"charadesc\">最年少で芥川賞を受賞した天才小説家。小説を書き続けるべきか悩んでいる。</div>"
+
+}
+
+function chara_2() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">京極太郎　<span style=\"font-size:20px\">キャスト：猿田真一郎</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_2.png\" width=\"300px\"></div><div class=\"charadesc\">次郎の兄。父親のせいで夢を諦め、次郎を守りながら生きるため借金取りになった。</div>"
+
+}
+
+function chara_3() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">テディベア　<span style=\"font-size:20px\">キャスト：柴崎煇</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_3.png\" width=\"300px\"></div><div class=\"charadesc\">芥川の昔から大切にしているテディベア。これがないと芥川は眠れない。</div>"
+
+}
+
+function chara_4() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">京極エリカ　<span style=\"font-size:20px\">キャスト：柴田瞳</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_4.png\" width=\"300px\"></div><div class=\"charadesc\">芥川の想い人。誰にでも優しく、清純派な可愛らしい女の子、と見せかけて…？</div>"
+
+}
+
+function chara_5() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">直木凌　<span style=\"font-size:20px\">キャスト：瀬下明紗子</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_5.png\" width=\"300px\"></div><div class=\"charadesc\">ジョジョの名言を呼吸するように発する女。中原に連れられ、芥川のアシスタントになる。</div>"
+
+}
+
+function chara_6() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">海　<span style=\"font-size:20px\">キャスト：田中美貴子</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_6.png\" width=\"300px\"></div><div class=\"charadesc\">人気急上昇中のアイドルグループ、わずらい♡(ha-to)のクールビューティー担当。女優になるか、アイドルを続けるか迷っている。</div>"
+
+}
+
+function chara_7() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">京極次郎　<span style=\"font-size:20px\">キャスト：德岡淳司</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_7.png\" width=\"300px\"></div><div class=\"charadesc\">芥川の借金を徴収にくる借金取り。ものすごくバカ。芥川の本の大ファン。</div>"
+
+}
+
+function chara_8() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">芥川の妹　<span style=\"font-size:20px\">キャスト：三品万麻紗</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_8.png\" width=\"300px\"></div><div class=\"charadesc\">芥川の悪夢に度々登場し、芥川を苦しめる。芥川の劣等感の強い表れ。</div>"
+
+}
+
+function chara_9() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">芥川の父/主人公　<span style=\"font-size:20px\">キャスト：山田兼也</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_9.png\" width=\"300px\"></div><div class=\"charadesc\">芥川の悪夢に度々登場し、芥川を苦しめる。芥川自身、芥川の憧れの強い表れ。</div>"
+
+}
+
+function chara_10() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">桃香　<span style=\"font-size:20px\">キャスト：脇田はるか</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_10.png\" width=\"300px\"></div><div class=\"charadesc\">人気急上昇中のアイドルグループ、わずらい♡(ha-to)のメンバー。ぶりっ子キャラだが本当は元ヤン。</div>"
+
+}
+
+function chara_11() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">夏希　<span style=\"font-size:20px\">キャスト：渡辺晏夏</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_11.png\" width=\"300px\"></div><div class=\"charadesc\">人気急上昇中のアイドルグループ、わずらい♡(ha-to)のリーダー。責任感が強く、グループをどう売り出して行くか奮闘している。</div>"
+
+}
+
+function chara_12() {
+	document.getElementById('charainfo').innerHTML = "<span style=\"font-size:44px\">中原叶奈子　<span style=\"font-size:20px\">キャスト：但野仁美</span></span><br /><div class=\"charaimage\"><img src=\"img/cast/cast_12.png\" width=\"300px\"></div><div class=\"charadesc\">芥川の担当編集であり、良き理解者。</div>"
+
+}
